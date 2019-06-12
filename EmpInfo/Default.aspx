583e0544b674327a2b805c46228a8113ce64408d
unknown <rgreene@SY-PA-FL-001.ers.local>
2019-06-12 12:38:57 -0400

    Initial commit

---
+++ EmpInfo/Default.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EmpInfo.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Employee Information</h2>
+        </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            Name:
+            <asp:TextBox ID="TxtName" runat="server" OnTextChanged="TxtName_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Select Office:&nbsp; <asp:DropDownList ID="DropList" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem>Pick City</asp:ListItem>
+                <asp:ListItem>Toronto</asp:ListItem>
+                <asp:ListItem>New York</asp:ListItem>
+                <asp:ListItem>London</asp:ListItem>
+                <asp:ListItem></asp:ListItem>
+            </asp:DropDownList>
+&nbsp;
+            <asp:Label ID="LblResult" runat="server"></asp:Label>
+        </p>
+        <p>
+            Department:</p>
+        <p>
+            <asp:RadioButton ID="RdoMarketing" runat="server" GroupName="Department" Text="Marketing" />
+        </p>
+        <p>
+            <asp:RadioButton ID="RdoAccounting" runat="server" GroupName="Department" OnCheckedChanged="Radio_CheckedChanged" Text="Accounting" />
+        </p>
+        <p>
+            <asp:RadioButton ID="RdoLegal" runat="server" GroupName="Department" OnCheckedChanged="RadioButton3_CheckedChanged" Text="Legal" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Qualifications:</p>
+        <p>
+            <asp:CheckBox ID="ChkBABS" runat="server" Text="BA/BS" />
+        </p>
+        <p>
+            <asp:CheckBox ID="ChkMA" runat="server" Text="MA" />
+        </p>
+        <p>
+            <asp:CheckBox ID="ChkPHD" runat="server" Text="PHD" />
+        </p>
+        <p>
+            <asp:Button ID="ButtonSend" runat="server" OnClick="ButtonSend_Click" Text="Send" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="LblFullResult" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ EmpInfo/Default.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmpInfo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropList.SelectedIndex != 0)
+            {
+                LblResult.Text = DropList.SelectedValue;
+            }
+            else
+            {
+                LblResult.Text = "Please, choose a city.";
+            }
+        }
+
+        protected void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Radio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSend_Click(object sender, EventArgs e)
+        {
+
+            if (TxtName.Text != "" && DropList.SelectedIndex != 0)
+            {
+                LblFullResult.Text = $"{TxtName.Text} lives in {LblResult.Text}";
+
+                if (RdoMarketing.Checked)
+                {
+                    LblFullResult.Text += $" and works in {RdoMarketing.Text}";
+                }
+                else if (RdoAccounting.Checked)
+                {
+                    LblFullResult.Text += $" and works in {RdoAccounting.Text}";
+                }
+                else if (RdoLegal.Checked)
+                {
+                    LblFullResult.Text += $" and works in {RdoLegal.Text}";
+                }
+
+                if (!ChkBABS.Checked && !ChkMA.Checked && !ChkPHD.Checked)
+                {
+                    LblFullResult.Text += " without a degree.";
+                }
+                else if (ChkBABS.Checked && !ChkMA.Checked && !ChkPHD.Checked) //BABS
+                {
+                    LblFullResult.Text += $" with a {ChkBABS.Text}.";
+                }
+                else if (!ChkBABS.Checked && ChkMA.Checked && !ChkPHD.Checked) //MA
+                {
+                    LblFullResult.Text += $" with a {ChkMA.Text}.";
+                }
+                else if (!ChkBABS.Checked && !ChkMA.Checked && ChkPHD.Checked) //PHD
+                {
+                    LblFullResult.Text += $" with a {ChkPHD.Text}.";
+                }
+                else if (ChkBABS.Checked && ChkMA.Checked && !ChkPHD.Checked) //BABS + MA
+                {
+                    LblFullResult.Text += $" with a {ChkBABS.Text} and {ChkMA.Text}.";
+                }
+                else if (!ChkBABS.Checked && ChkMA.Checked && ChkPHD.Checked) //MA + PHD
+                {
+                    LblFullResult.Text += $" with a {ChkMA.Text} and {ChkPHD.Text}.";
+                }
+                else if (ChkBABS.Checked && !ChkMA.Checked && ChkPHD.Checked) //BABS + PHD
+                {
+                    LblFullResult.Text += $" with a {ChkBABS.Text} and {ChkPHD.Text}.";
+                }
+                else if (ChkBABS.Checked && ChkMA.Checked && ChkPHD.Checked) //BABS + MA + PHD
+                {
+                    LblFullResult.Text += $" with a {ChkBABS.Text}, {ChkMA.Text} and {ChkPHD.Text}.";
+                }
+            }
+            else
+            {
+                LblFullResult.Text = "Please, complete the required fields.";
+            }
+        }
+
+        protected void TxtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
